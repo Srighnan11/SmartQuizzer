543a44a03f494c67bc89c77d5942697363ba9243
Srighnan11 <srighnanreddy@gmail.com>
2026-03-16 14:25:26 +0530

    clean requirements file

--- backend/app.py
+++ backend/app.py
@@ -7407,4 +7407,5 @@ def delete_flag(flag_id):
 
 if __name__ == '__main__':
     # Ensure you are running MongoDB Atlas and have set your MONGO_URI in .env
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
